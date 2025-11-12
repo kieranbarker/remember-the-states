2f2557dbfbcce33a198463dacd3ed87c18ed02e6
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2025-11-12 17:48:55 +0000

    Setup simple deployment pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/remember-the-states/",
   plugins: [react()],
   test: {
     environment: "jsdom",
